a27b3260a9075d17de92e509e53f66b5f5cdd76b
Gabrel Leopoldino <gabrielperes97@gmail.com>
2018-11-12 16:52:36 -0200

    Interace crua finalizada. Falta as funções sobre clique e jogo.

--- app/src/main/java/com/leopoldino/gabriel/campominado/GameActivity.kt
+++ app/src/main/java/com/leopoldino/gabriel/campominado/GameActivity.kt
@@ -2,11 +2,12 @@ package com.leopoldino.gabriel.campominado
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.LinearLayout
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
+import java.util.*
+import kotlin.collections.ArrayList
 
 class GameActivity: AppCompatActivity() {
 
@@ -20,6 +21,40 @@ class GameActivity: AppCompatActivity() {
 
     lateinit var grid : TableLayout
 
+    inner class Ponto(val x: Int, val y : Int) : Button(this) {
+        var bombasProximas = 0
+        set(value) {
+            field = value
+            if (text != "X")
+                text = value.toString()
+        }
+        var bomba = false
+        set(value) {
+            field = value
+            if (value)
+                text = "X"
+        }
+
+        init {
+            super.setOnClickListener{view ->
+                val clickedButton = view as Ponto
+                if (!clickedButton.bomba)
+                {
+                    //Abre os sem bomba juntos
+                    //ou abre numero
+                }
+                else
+                {
+                    //game over
+                }
+
+            }
+        }
+    }
+
+    lateinit var campo : Array<Array<Ponto>>
+    lateinit var bombas : Array<Ponto>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -27,27 +62,34 @@ class GameActivity: AppCompatActivity() {
         difficult = intent.getSerializableExtra("Difficult") as DIFFICULT
         grid = findViewById(R.id.tl_game_grid)
 
-        when(difficult){
+        campo = when(difficult){
             DIFFICULT.EASY -> makeButtons(7)
             DIFFICULT.MEDIUM -> makeButtons(10)
             DIFFICULT.HARD -> makeButtons(13)
         }
 
+        bombas = when(difficult){
+            DIFFICULT.EASY -> putBombs(7, 7)
+            DIFFICULT.MEDIUM -> putBombs(10, 10)
+            DIFFICULT.HARD -> putBombs(13, 13)
+        }
+
 
     }
 
-    fun makeButtons(size : Int){
+    fun makeButtons(size : Int)  : Array<Array<Ponto>>{
 
         grid.removeAllViewsInLayout()
 
-        for (i in 1..size) {
+        val list = Array<Array<Ponto>>(size){i : Int ->
+
             val row = TableRow(this)
             val row_layout = TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT)
             row_layout.setMargins(0, -9, 0, -3)
             row.layoutParams = row_layout
 
-            for (j in 1..size){
-                val button = Button(this)
+            val pontos = Array<Ponto>(size){j : Int ->
+                val button = Ponto(i, j)
                 val layout = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
                 layout.weight = 1f
 
@@ -63,9 +105,65 @@ class GameActivity: AppCompatActivity() {
 
                 button.text = "0"
                 row.addView(button)
+
+
+                button
             }
             grid.addView(row)
+
+            pontos
         }
 
+        return list
+    }
+
+    fun putBombs(many : Int, bound : Int) : Array<Ponto> {
+        val random = Random()
+        val bombs = Array<Ponto>(many) {
+
+            var x = 0
+            var y = 0
+            do {
+                x = random.nextInt(bound)
+                y = random.nextInt(bound)
+            } while (campo[x][y].bomba)
+            campo[x][y].bomba = true
+
+            //Cima
+            if (x-1 >= 0)
+            {
+                //Diagonal superior esqueda
+                if (y-1 >= 0)
+                    campo[x-1][y-1].bombasProximas++
+                //Superior
+                campo[x-1][y].bombasProximas++
+                //Diagonal superior direita
+                if (y+1 < bound)
+                    campo[x-1][y+1].bombasProximas++
+            }
+            // --Mesma linha--
+            //esquerda
+            if (y-1 >= 0)
+                campo[x][y-1].bombasProximas++
+            //direita
+            if (y+1 < bound)
+                campo[x][y+1].bombasProximas++
+
+            //Baixo
+            if (x+1 < bound)
+            {
+                //Diagonal inferior esqueda
+                if (y-1 >= 0)
+                    campo[x+1][y-1].bombasProximas++
+                //Superior
+                campo[x+1][y].bombasProximas++
+                //Diagonal inferior direita
+                if (y+1 < bound)
+                    campo[x+1][y+1].bombasProximas++
+            }
+             campo[x][y]
+        }
+
+        return bombs
     }
 }
